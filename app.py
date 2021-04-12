4fb198a35b7fa05571593b72eedf6890136acd2c
robo-codes <61969933+robo-codes@users.noreply.github.com>
2021-04-12 15:40:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def predict():
     prediction = model.predict(final_features)
     print(final_features)
     output = round(prediction[0], 2)
-    return jsonify(output)
+    return jsonify({"Result" : output})
 
 if __name__ == "__main__":
     app.run(debug=True)
